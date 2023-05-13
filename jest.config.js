e9fe825421f898da701f609b6b6a684e8f426305
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-12 21:16:17 -0400

    perf(jest): set coverage report to all files instead of list of selected ones

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,13 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
-const COVERAGE_FILE_SUFFIX = ['module', 'service', 'util', 'decorator'];
-
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   rootDir: '.',
   moduleFileExtensions: ['js', 'json', 'ts'],
   testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  collectCoverageFrom: [`**/*.(${COVERAGE_FILE_SUFFIX.join('|')}).ts`],
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  collectCoverageFrom: ['src/**/*.*.ts'],
   coverageDirectory: './coverage',
   testEnvironment: 'node',
   // Helps to use aliases in tsconfig (@module/*)
